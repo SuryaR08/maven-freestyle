80f7ad491fd44d4fb3804984d337c572fcfda2bc
surya <suryar.aids2021@citchennai.net>
2024-12-28 12:09:21 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args) 
     {
-        string API = 'ksdldkjdlsdjldn-sdkdsj';
-        List<String> a = new ArrayList<>();
-        System.out.println( "Hello I am jenkins-maven-freestyle project!" );
+        String API = "ksdldkjdlsdjldn-sdkdsj";  
+        List<String> a = new ArrayList<>();    
+        System.out.println("Hello I am jenkins-maven-freestyle project!");
     }
 }
